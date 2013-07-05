68fe5acbb468604804f7b2c108b82da90ee32c8a
Chris Kelly <cpjk@ananke.bioc.uvic.ca>
2013-07-05 11:02:37 -0700

    initial commit

---
+++ getlongestseq.py
@@ -0,0 +1,26 @@
+from sys import argv, exit
+
+def main():
+    infile = open(argv[1]) if len(argv) >= 2 else exit("Must use at least 2 arguments")
+    print "%s" % findlongest(infile)
+
+
+def findlongest(f):
+    """Return the length of the longest sequence as a unicode string"""
+    longestlength = 0
+    currentseqlength = 0
+
+    for line in f.readlines():
+        if line[0] == ">":
+            if currentseqlength > longestlength:
+                longestlength = currentseqlength
+            currentseqlength = 0
+            continue
+        else:
+            currentseqlength += len(line.rstrip())
+
+    return longestlength
+
+
+if __name__ == "__main__":
+    main()
