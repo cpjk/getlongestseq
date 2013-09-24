1804220769309f8b95e9ec07fa99642824ea307d
Chris Kelly <cpjkelly@gmail.com>
2013-09-23 21:46:46 -0700

    removed extraneous blank lines

--- getlongestseq.py
+++ getlongestseq.py
@@ -1,11 +1,9 @@
 from sys import argv, exit
 
-
 def main():
     infile = open(argv[1]) if len(argv) >= 2 else exit("Usage: getlongestseq.py [FASTA file]")
     length, name = findlongest(infile)
     print "%s: %s" % (length, name)
-
 
 def findlongest(f):
     """Return the length of the longest sequence and its id line as a tuple"""
@@ -14,7 +12,6 @@ def findlongest(f):
     oldseqname = None
     newseqname = None
     longestseqname = None
-
     for line in f.readlines():
         if line[0] == ">":
             oldseqname, newseqname = newseqname, line[1:].rstrip()
@@ -25,9 +22,7 @@ def findlongest(f):
             continue
         else:
             currentseqlength += len(line.rstrip())
-
     return (longestlength, longestseqname)
-
 
 if __name__ == "__main__":
     main()
